5db756fa286847c5806f04ba75e103edda457e53
znbnzb <1524922080@qq.com>
2018-08-27 14:42:57 +0800

    提交文件

---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="translateForm">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'translateForm'
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
